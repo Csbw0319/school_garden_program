a0363607bb16faa55243b0a3f1fab06c1ad7eecc
Csbw0319 <cwilkins0319@icloud.com>
2022-08-26 21:30:16 -0400

    lots of styling

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'school_garden'
+    'school_garden',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
